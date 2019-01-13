cec31aad62af7c521d38503aacf2a143424ea905
JungHo <cjsgh85@gmail.com>
2019-01-13 19:20:02 +0900

    butActivity init
    create java file and layout xml and add some test image

--- app/src/main/java/godock/edenhighway/sujae/com/godock/BuyActivity.java
+++ app/src/main/java/godock/edenhighway/sujae/com/godock/BuyActivity.java
@@ -1,4 +1,13 @@
 package godock.edenhighway.sujae.com.godock;
 
-public class BuyActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BuyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
