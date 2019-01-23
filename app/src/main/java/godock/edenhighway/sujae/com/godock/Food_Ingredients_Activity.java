f45acab95b486216adab9cd96a802e6ec2b14169
JungHo <cjsgh85@gmail.com>
2019-01-23 20:23:57 +0900

    add layout
    food_ingredient_activity and category pop-up

--- app/src/main/java/godock/edenhighway/sujae/com/godock/Food_Ingredients_Activity.java
+++ app/src/main/java/godock/edenhighway/sujae/com/godock/Food_Ingredients_Activity.java
@@ -1,4 +1,67 @@
 package godock.edenhighway.sujae.com.godock;
 
-public class makeActivity {
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Food_Ingredients_Activity extends AppCompatActivity  {
+    private Button viewlistButton;
+    private Button addButton;
+    private Button makeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_ingredients_activity);
+
+        viewlistButton = findViewById(R.id.viewlistButton);
+        addButton = findViewById(R.id.addButton);
+        makeButton = findViewById(R.id.makeButton);
+
+
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.viewlistButton:
+                        // 목록으로 보기 구현
+                        break;
+                    case R.id.addButton:
+                        // 재료 추가 팝업 구현
+                        View textView = View.inflate(getApplicationContext(), R.layout.addpopup, null);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                        builder.setTitle("재료 추가");
+                        builder.setMessage("추가할 재료의 이름을 입력해주세요.")
+                                .setView(textView)
+                                .setCancelable(false)
+                                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Intent intent = new Intent(getApplicationContext(), BuyActivity.class);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setNegativeButton("돌아가기", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                }).show();
+                        break;
+                    case R.id.makeButton:
+                        Intent intent = new Intent(getApplicationContext(), MakeActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        };
+        viewlistButton.setOnClickListener(buttonListener);
+        addButton.setOnClickListener(buttonListener);
+        makeButton.setOnClickListener(buttonListener);
+
+
+    }
+
 }
--- app/src/main/java/godock/edenhighway/sujae/com/godock/MainActivity.java
+++ app/src/main/java/godock/edenhighway/sujae/com/godock/MainActivity.java
@@ -1,32 +1,88 @@
 package godock.edenhighway.sujae.com.godock;
 
-import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private ImageButton buybutton;
+    private ImageButton buyButton;
+    private ImageButton makeButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buybutton = (ImageButton)findViewById(R.id.buy);
-        buybutton.setOnClickListener(this);
+        buyButton = (ImageButton)findViewById(R.id.buy);
+        buyButton.setOnClickListener(this);
+
+        makeButton = findViewById(R.id.make);
+        makeButton.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.buy:
-                Intent intent = new Intent(getApplicationContext(), BuyActivity.class);
+                View checkBoxView = View.inflate(this, R.layout.category_checkboxes, null);
+                CheckBox checkBox1 = (CheckBox) checkBoxView.findViewById(R.id.checkbox1);
+                CheckBox checkBox2 = (CheckBox) checkBoxView.findViewById(R.id.checkbox2);
+                CheckBox checkBox3 = (CheckBox) checkBoxView.findViewById(R.id.checkbox3);
+                CheckBox checkBox4 = (CheckBox) checkBoxView.findViewById(R.id.checkbox4);
+                CheckBox checkBox5 = (CheckBox) checkBoxView.findViewById(R.id.checkbox5);
+                CheckBox checkBox6 = (CheckBox) checkBoxView.findViewById(R.id.checkbox6);
+
+                checkBox1.setOnCheckedChangeListener(CheckBoxListener);
+                checkBox1.setText("한식");
+                checkBox2.setOnCheckedChangeListener(CheckBoxListener);
+                checkBox2.setText("중식");
+                checkBox3.setOnCheckedChangeListener(CheckBoxListener);
+                checkBox3.setText("일식");
+                checkBox4.setOnCheckedChangeListener(CheckBoxListener);
+                checkBox4.setText("양식");
+                checkBox5.setOnCheckedChangeListener(CheckBoxListener);
+                checkBox5.setText("안주류");
+                checkBox6.setOnCheckedChangeListener(CheckBoxListener);
+                checkBox6.setText("다이어트");
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("카테고리 설정");
+                builder.setMessage("카테고리를 설정해주세요!")
+                        .setView(checkBoxView)
+                        .setCancelable(false)
+                        .setPositiveButton("결과보기", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent intent = new Intent(getApplicationContext(), BuyActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton("돌아가기", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }).show();
+                break;
+
+            case R.id.make:
+                Intent intent = new Intent(getApplicationContext(), Food_Ingredients_Activity.class);
                 startActivity(intent);
                 break;
         }
     }
+
+    CompoundButton.OnCheckedChangeListener CheckBoxListener = new CompoundButton.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            // 체크박스 선택시 해당 카테고리를 참조해서 결과 보여주도록 함.
+        }
+    };
 }
